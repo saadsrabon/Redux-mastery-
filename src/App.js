0d5cba93ba42a03366c9b9ebf18d658932541885
Saad Srabon <saadsrabon2@gmail.com>
2023-12-13 13:39:53 +0600

    used connect api to understand the process

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import { Provider } from "react-redux";
 import Counter from "./components/Counter";
-
+import store from "./redux/store";
 
 export default function App() {
     return (
-      
+        <Provider store={store}>
             <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
                 <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                     Simple Counter Application
@@ -14,6 +14,6 @@ export default function App() {
                     <Counter id="1" />
                 </div>
             </div>
-       
+        </Provider>
     );
 }
